08b6bdc0ccc85f0ac62205ee8976e7aa6af35f1e
water <water@localhost.localdomain>
2018-05-21 10:33:11 +0800

    -all

---
+++ zhushi/zhushi.py
@@ -0,0 +1,24 @@
+#我注释了一行
+
+#注释多行？
+
+''' 注释多行
+123456
+jlkjl
+记录会计分录k
+'''
+"""
+jlkfjlkj
+jflkajld
+fkaljfdlk
+
+"""
+
+
+print("hello world")
+
+print(19)
+
+print(19 + 232)
+
+print("10 + 8 =", 18)
